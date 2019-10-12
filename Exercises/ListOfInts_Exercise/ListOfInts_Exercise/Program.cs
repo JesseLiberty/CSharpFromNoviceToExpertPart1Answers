38d187f9561461ef2fa4abc3d39c331fcee74661
Jesse Liberty <jesseliberty@gmail.com>
2019-10-12 13:26:05 -0400

    generic exercise

---
+++ Exercises/ListOfInts_Exercise/ListOfInts_Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListOfInts_Exercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var myList = new List<int>
+         {
+            1, 3, 5, 7, 9, 11, 13, 15
+         };
+
+         foreach (var theInt in myList)
+         {
+            Console.WriteLine(theInt);
+         }
+      }
+   }
+}
