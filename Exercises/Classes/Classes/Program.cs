148fb72b4518c11b044eb1c05ce3939c93e09cf5
Jesse Liberty <jesseliberty@gmail.com>
2019-10-12 13:43:48 -0400

    classes

---
+++ Exercises/Classes/Classes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Classes
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var manufacturer = new Manufacturer();
+         manufacturer.SetCar();
+      }
+   }
+
+   class Car
+   {
+      public int NumberOfWheels { get; set; }
+      public int NumberOfHorns { get; set; }
+
+      public override string ToString()
+      {
+         return $"I have {NumberOfWheels} wheels and {NumberOfHorns} horn.";
+      }
+   }
+
+   class Manufacturer
+   {
+      public string Name { get; set; }
+
+      public void SetCar()
+      {
+         var car = new Car();
+         car.NumberOfHorns = 1;
+         car.NumberOfWheels = 4;
+
+         Console.WriteLine(car);
+      }
+   }
+}
