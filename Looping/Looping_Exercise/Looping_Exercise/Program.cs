c2210e04dde455777b8bb138ef1499c2b660be03
Jesse Liberty <jesseliberty@gmail.com>
2019-10-12 13:13:11 -0400

    Looping exercise

---
+++ Looping/Looping_Exercise/Looping_Exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Looping_Exercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         for (var i = 1; i < 11; i++)
+         {
+            Console.WriteLine(i);
+         }
+      }
+   }
+}
